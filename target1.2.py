c4a3bcb69576c6f1d75718525436469168da1063
MortemAfro <54048027+MortemAfro@users.noreply.github.com>
2022-11-24 17:39:21 -0500

    Create target1.2.py

---
+++ target1.2.py
@@ -0,0 +1,9 @@
+
+
+if __name__=="__main__":
+    x=2022
+    #Parte a)
+    inicio1= time.perf_counter()
+    resultado_serial= funcion(x)
+    final1= time.perf_counter()
+    tiempo1=final1-inicio1
